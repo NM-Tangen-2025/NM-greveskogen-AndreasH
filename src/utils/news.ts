3f03089fe0f768c4b661579cc92717d51ae89d6b
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 11:34:21 +0200

    Added server side function for fetching specific article

--- src/utils/news.ts
+++ src/utils/news.ts
@@ -26,3 +26,32 @@ export async function getNews() {
     return { success: false, error: 'Failed to fetch news data' };
   }
 }
+
+export async function getNewsById(id: string) {
+  try {
+    const response = await fetch(
+      `https://fortunate-bear-715099df12.strapiapp.com/api/Nyhetsartikler/${id}?populate=*`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Authorization': `Bearer ${process.env.CMS_API_KEY}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    // console.log("Recieved data:", data); 
+
+    return { success: true, data };
+  } catch (error) {
+    console.error('Error fetching news data:', error);
+    return { success: false, error: 'Failed to fetch news data' };
+  }
+}
