480d62d4a7395935fa93d7adaa53b46591c6aa4c
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 10:06:14 +0200

    Added own api for getting news from strapi cms using previously made function

---
+++ app/api/news/route.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { NextResponse } from 'next/server';
+import {getNews} from '@/src/utils/news';
+export async function GET() {
+  try {
+    console.log('Getting news articles from strapi...');
+    const allNews = await getNews();
+
+    const responseData = allNews;
+
+
+
+    if (allNews.success == false) {
+      return NextResponse.json(
+        { error: 'Internal server error' },
+        { status: 500 }
+      );
+    }
+    
+    return NextResponse.json({ status: 200, data: responseData.data });
+  } catch (error) {
+    console.error('Error from Getting news articles', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
