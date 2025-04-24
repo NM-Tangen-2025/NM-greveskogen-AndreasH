22df5fe87f5f61e16e3ed3e352e3ea1037d04dce
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 12:37:00 +0200

    Made types for arrangementer

--- src/types/arrangementer.ts
+++ src/types/arrangementer.ts
@@ -1,9 +1,63 @@
 export interface arrangementer {
-  arrangementstype: string;
-  tittel: string;
-  ingress: string;
-  beskrivelse: string;
-  lokasjon: string;
-  dato: string;
-  bilde: string;
+  id: string;
+  documentId: string;
+  Type: string;
+  Tittel: string;
+  Tidspunkt: string;
+  Ingress: string;
+  Beskrivelse: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  Lokasjon: Lokasjon;
+  Bilde: Bilde;
+}
+
+export interface Lokasjon {
+  id: string;
+  documentId: string;
+  Navn: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
+
+interface Bilde {
+  id: number;
+  documentId: string;
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: {
+    large: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+    };
+    small: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+    };
+    medium: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+    };
+    thumbnail: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+    };
+  };
 }
