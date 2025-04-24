1814baa32b0a0b540a5fc11ced835183b3918116
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 09:37:58 +0200

    Updated layout.tsx with new footer, navbar and updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,30 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from '@/src/components/footer/footer';
+import Navbar from '@/src/components/navbar/Navbar';
+import {
+  FaNewspaper,
+  FaHome,
+  FaInfoCircle,
+  FaEnvelope,
+  FaShoppingBag,
+} from 'react-icons/fa';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Skole-NM 2025",
-  description: "Nettside laget av Andreas Høiås Ugelstad for Skole-NM 2025",
+  title: 'Tall Ships Races 2025',
+  description: 'Nettside for Tall Ships Races 2025',
+  icons: '/favicon.ico',
 };
 
 export default function RootLayout({
@@ -22,10 +32,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navItems = [
+    { name: 'Home', path: '/', icon: <FaHome /> },
+    { name: 'News', path: '/news', icon: <FaNewspaper /> },
+    { name: 'Products', path: '/products', icon: <FaShoppingBag /> },
+    { name: 'About', path: '/about', icon: <FaInfoCircle /> },
+    { name: 'Contact', path: '/contact', icon: <FaEnvelope /> },
+  ];
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+              <Navbar
+        logo={{
+          src: '/logo.svg',
+          alt: 'Tall Ships Races logo',
+          width: 204,
+          height: 81,
+        }}
+        brandName="Apinor"
+        items={navItems}
+        showCart={true}
+        showBanner={true}
+      />
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
