4a82d3bcc19342f1287090270f792f35218d4399
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 12:30:20 +0200

    Added server side function to getevents from strapi

---
+++ src/utils/events.ts
@@ -0,0 +1,28 @@
+export async function getEvents() {
+      try {
+    const response = await fetch(
+      `https://fortunate-bear-715099df12.strapiapp.com/api/Arrangementer?populate=*`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Authorization': `Bearer ${process.env.CMS_API_KEY}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log('Arrangementer: ', data); 
+
+    return { success: true, data };
+  } catch (error) {
+    console.error('Error fetching news data:', error);
+    return { success: false, error: 'Failed to fetch news data' };
+  }
+}
