1459fcea389053df61a2cd9f69007f45796ec348
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 10:05:06 +0200

    Made news function to fetch all news from api

---
+++ src/utils/news.ts
@@ -0,0 +1,28 @@
+export async function getNews() {
+  try {
+    const response = await fetch(
+      `https://fortunate-bear-715099df12.strapiapp.com/api/Nyhetsartikler`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Authorization': `Bearer ${process.env.CMS_API_KEY}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    // console.log('News data:', data); 
+
+    return { success: true, data };
+  } catch (error) {
+    console.error('Error fetching news data:', error);
+    return { success: false, error: 'Failed to fetch news data' };
+  }
+}
