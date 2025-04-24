643f72571b8e8a581502ebd4641fe0a480541893
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-04-24 10:06:33 +0200

    Made nyheter page call own api at api/news

--- app/nyheter/page.tsx
+++ app/nyheter/page.tsx
@@ -1,4 +1,27 @@
+'use client'
+import { useEffect } from "react";
 export default function Page(){
+    async function GetNews() {
+    try {
+      const tempAPI = await fetch(`/api/news/`, {
+        method: 'GET',
+      });
+
+      const responseData = await tempAPI.json();
+
+      console.log("Responsedata from nyhetsapi: ", responseData)
+
+      if (tempAPI.ok) {
+        // Sucessfull request - handle accordingly
+      }
+    } catch (error) {
+      console.error("Error while fetching URL", error)
+    }
+  }
+    useEffect(() => {
+        GetNews()
+  }, []);
+  
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-4xl font-bold mb-4">Nyheter</h1>
